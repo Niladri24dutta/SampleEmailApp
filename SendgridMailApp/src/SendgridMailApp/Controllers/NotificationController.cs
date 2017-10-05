74a296e8b9ac17dfed5039eeea1de898d643f22b
Niladri28dutta <nild2141@gmail.com>
2017-10-06 01:56:33 +0530

    initial commit added new controller

---
+++ SendgridMailApp/src/SendgridMailApp/Controllers/NotificationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+
+namespace SendgridMailApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotificationController : Controller
+    {
+
+        // POST api/values
+        [HttpPost(Name ="SendEmail")]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        
+    }
+}
