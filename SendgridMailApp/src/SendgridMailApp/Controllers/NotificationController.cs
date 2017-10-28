9293cd9f5a9df18d0982dad3a5b12d0cc7794c90
Niladri28dutta <nild2141@gmail.com>
2017-10-29 01:21:15 +0530

    initial commit

--- SendgridMailApp/src/SendgridMailApp/Controllers/NotificationController.cs
+++ SendgridMailApp/src/SendgridMailApp/Controllers/NotificationController.cs
@@ -5,19 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-
-
+using Microsoft.Extensions.Configuration;
 
 namespace SendgridMailApp.Controllers
 {
     [Route("api/[controller]")]
     public class NotificationController : Controller
     {
+        private readonly IConfiguration _configuration;
 
-        // POST api/values
-        [HttpPost(Name ="SendEmail")]
-        public void Post([FromBody]string value)
+        public NotificationController(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+        [HttpPost(Name ="SendEmail")]
+        public async Task Post([FromBody]string value)
+        {
+            var apiKey = _configuration.GetSection("SENDGRID_API_KEY").Value;
+            //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("noreply@example.com", "Sendgrid user");
+            var subject = "Hello world email from Sendgrid ";
+
+            var to = new EmailAddress("tests@mail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
 
         
--- SendgridMailApp/src/SendgridMailApp/Startup.cs
+++ SendgridMailApp/src/SendgridMailApp/Startup.cs
@@ -29,6 +29,7 @@ namespace SendgridMailApp
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
